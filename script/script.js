5506996e7ec23dc576ce8eca9fb7a23bee776e15
Yauheny Bychkou <375445433672@yandex.by>
2021-07-30 14:35:42 +0300

    lesson02 dificult

--- script/script.js
+++ script/script.js
@@ -1,8 +1,11 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
-console.log('JS course');
-alert('JS course');
+let num = 266219;
+let sum = 1;
+let deg = 3;
+while(num >= 1) {
+  sum *= num % 10;
+  num = Math.floor(num / 10);
+}
+console.log('2*6*6*2*1*9= ', sum);
+let rez = sum ** deg;
+console.log('1296^3=', rez);
+console.log(String(rez).slice(0,2));
