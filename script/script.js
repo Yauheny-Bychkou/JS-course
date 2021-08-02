e93d58e0f0b9aa6eef061cc7127a7e4df7b22c43
Yauheny Bychkou <375445433672@yandex.by>
2021-08-02 19:02:39 +0300

    code style

--- script/script.js
+++ script/script.js
@@ -1,19 +1,18 @@
-let money = prompt('Ваш месячный доход?');
 let income = 'freelance';
+let mission = 1000000;
+let period = 7;
+let money = prompt('Ваш месячный доход?');
 let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
 let deposit = confirm('Есть ли у вас депозит в банке?');
 let expenses1 = prompt('Введите обязательную статью расходов');
 let amount1 = prompt('Во сколько это обойдется?');
 let expenses2 = prompt('Введите обязательную статью расходов');
 let amount2 = prompt('Во сколько это обойдется?');
+
 let budgetMonth = money - amount1 - amount2;
-console.log('budgetMonth: ', budgetMonth);
-let mission = 1000000;
-let period = 7;
 let purpose = Math.ceil(mission / budgetMonth);
-console.log('purpose, месяцев: ', purpose);
 let budgetDay = Math.floor(budgetMonth/30);
-console.log('budgetDay: ', budgetDay);
+
 if(budgetDay >= 1200) {
   console.log('У вас очень высокий доход');
 } else if(budgetDay >= 600){ 
@@ -21,6 +20,10 @@ if(budgetDay >= 1200) {
 } else if(budgetDay >= 0){ 
   console.log('К сожалению у вас уровень дохода ниже среднего');
 } 
+
+console.log('budgetMonth: ', budgetMonth);
+console.log('budgetDay: ', budgetDay);
+console.log('purpose, месяцев: ', purpose);
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
