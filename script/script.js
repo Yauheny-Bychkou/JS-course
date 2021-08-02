33ca8796f3c554bb2aa0ba42a9b0ca38322aabd3
Yauheny Bychkou <375445433672@yandex.by>
2021-08-02 14:40:51 +0300

    lesson03

--- script/script.js
+++ script/script.js
@@ -1,9 +1,26 @@
-let money = 1500;
+let money = prompt('Ваш месячный доход?');
 let income = 'freelance';
-let addExpenses = 'taxi, ethernet, communal';
-let deposit = true;
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+let deposit = confirm('Есть ли у вас депозит в банке?');
+let expenses1 = prompt('Введите обязательную статью расходов');
+let amount1 = prompt('Во сколько это обойдется?');
+let expenses2 = prompt('Введите обязательную статью расходов');
+let amount2 = prompt('Во сколько это обойдется?');
+let budgetMonth = money - amount1 - amount2;
+console.log('budgetMonth: ', budgetMonth);
 let mission = 1000000;
 let period = 7;
+let purpose = Math.ceil(mission / budgetMonth);
+console.log('purpose, месяцев: ', purpose);
+let budgetDay = Math.floor(budgetMonth/30);
+console.log('budgetDay: ', budgetDay);
+if(budgetDay >= 1200) {
+  console.log('У вас очень высокий доход');
+} else if(budgetDay >= 600){ 
+  console.log('У вас средний уровень дохода');
+} else if(budgetDay >= 0){ 
+  console.log('К сожалению у вас уровень дохода ниже среднего');
+} 
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
@@ -11,5 +28,3 @@ console.log(addExpenses.length);
 console.log('The period is', period, 'mounth and purpose make', mission, 'dollars');
 console.log(addExpenses.toLowerCase);
 console.log(addExpenses.split(', '));
-let budgetDay = money/30;
-console.log('budgetDay: ', budgetDay);
